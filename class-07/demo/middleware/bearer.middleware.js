b578d4b9ad610199a8e0a185cd90c04cd84cfd83
thamudi <tamim.hamoudi@gmail.com>
2021-11-08 12:56:39 +0200

    class 07 demo

---
+++ class-07/demo/middleware/bearer.middleware.js
@@ -0,0 +1,20 @@
+'use strict';
+const Users = require('../models/users.model');
+
+function bearer(req, res, next) {
+
+  //Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRhbWltIiwiaWF0IjoxNjM2MzY2MDgwfQ.OhHLD4yRWs1LlTloBjIs0j-QYzi8LdoQDXUfPaO0BSg
+  console.log(req.headers.authorization);
+  const bearerHeaderToken = req.headers.authorization.split(' ')[1]; // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRhbWltIiwiaWF0IjoxNjM2MzY2MDgwfQ.OhHLD4yRWs1LlTloBjIs0j-QYzi8LdoQDXUfPaO0BSg
+
+  // take that token and check if the bearer is authenticated!
+  Users.authenticateBearer(bearerHeaderToken).then(userData => {
+    req.user = userData;
+    next();
+  }).catch(() => {
+    next('Bearer token authentication error');
+  });
+
+}
+
+module.exports = bearer;
--- class-07/demo/server.js
+++ class-07/demo/server.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 // 3rd Party Resources
 const express = require('express');
 const bcrypt = require('bcrypt');
-const Users = require('./models/usersModel.js');
-const basic = require('./middleware/basic.js');
-const bearer = require('./middleware/bearer.js');
+const Users = require('./models/users.model');
+const bearer = require('./middleware/bearer.middleware');
+const basic = require('./middleware/basic.middleware');
 
 // Prepare the express app
 const app = express();
@@ -17,9 +17,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/sign-up', async (req, res) => {
+  try {
+    // hash the password within the req body
+    req.body.password = await bcrypt.hash(req.body.password, 5);
+    // create the new user Record 
+    const record = await Users.create(req.body);
+    res.status(201).json(record);
+  } catch (error) {
+    res.status(403).send("Error occurred");
+  }
+});
+
+app.get('/user', bearer, (req, res) => {
+  res.json({
+    'message': 'You are authorized to view the user profile',
+    'user': req.user
+  });
 });
 
 app.post('/sign-in', basic, async (req, res) => {
+  res.status(200).json(req.user);
 });
 
 
