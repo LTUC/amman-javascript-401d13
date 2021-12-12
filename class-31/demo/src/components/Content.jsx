588beeeb41ba9a4bcd17031e785efde18beca297
thamudi <tamim.hamoudi@gmail.com>
2021-12-12 06:28:23 +0200

    Baisc Example structure

---
+++ class-31/demo/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Content extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Content
